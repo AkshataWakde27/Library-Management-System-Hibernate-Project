f4136d7edb6dc86677ab98e42bafc8a7df4679f7
AkshataWakde27 <akshatawakde@gmail.com>
2023-10-18 20:31:57 +0530

    success

---
+++ src/main/java/com/jsp/library/controller/AdminUpdate.java
@@ -0,0 +1,13 @@
+package com.jsp.library.controller;
+
+import com.jsp.library.service.AdminService;
+
+public class AdminUpdate {
+
+	public static void main(String[] args) {
+
+		AdminService adminservice = new AdminService();
+		adminservice.updateAdminUserNameByIdPass(1, "shraddha");
+
+	}
+}
---
+++ src/main/java/com/jsp/library/controller/LibriainGetAll.java
@@ -0,0 +1,12 @@
+package com.jsp.library.controller;
+
+import com.jsp.library.service.LibriainService;
+
+public class LibriainGetAll {
+	public static void main(String[] args) {
+		LibriainService libriainService = new LibriainService();
+
+		libriainService.getAllLibrarians();
+
+	}
+}
